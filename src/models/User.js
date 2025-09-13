5e36247d4b777737bde2c64646c0750d7a57c666
rasheed <rasheedjawula18@gmail.com>
2025-09-13 16:39:47 +0000

    Created and defined the User object

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+export class User {
+	constructor(name, email,id){
+		this.name = name;
+		this.email= email;
+		this.id = id;
+
+	}
+
+	/*static Id(){
+		return new Date().toString(36);
+	}*/
+
+
+
+}
+
+
+
+
+
+
